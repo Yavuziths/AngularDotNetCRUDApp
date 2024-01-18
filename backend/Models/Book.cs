6fc4cc4bd8c07baea81cde4a069e595b741463d0
Yavuziths <112006522+Yavuziths@users.noreply.github.com>
2024-01-18 09:59:27 +0100

    lagt till olika filer o mappar som kommer behövas i projektet

---
+++ backend/Models/Book.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace backend.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
